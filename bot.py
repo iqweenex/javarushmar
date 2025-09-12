afa8a6d241329d727392dc27e091e6eb5f3adf67
iqweenex <iqweenex@gmail.com>
2025-09-12 17:34:38 +0500

    12.09 bot

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
+import time
+
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
 
 from gpt import *
 from util import *
+
+dic = {
+    "date_grande": "Ариана Грандэ",
+    "date_robbie": "Марго Робби",
+    "date_zendaya": "Зендея",
+    "date_gosling": "Райан Гослинг",
+    "date_hardy": "Том Харди",
+}
+
+list_think = ["Обработка запроса.", "Обработка запроса..", "Обработка запроса...", "Обработка запроса...."]
 
 
 async def start(update, context):
@@ -37,29 +49,66 @@ async def date(update, context):
     dialog.mode = "date"
     text = load_message("date")
     await send_photo(update, context, "date")
-    await send_text_buttons(update, context, text, {
-        "date_ariana": "Ариана Грандэ",
-        "date_robbie": "Марго Робби",
-        "date_zandeya": "Зендея",
-        "date_gosling": "Райан Гослинг",
-        "date_hardy": "Том Харди",
-
-    })
+    await send_text_buttons(update, context, text, dic)
 
 
 async def date_dialog(update, context):
-    pass
+    text = update.message.text
+    my_message = await send_text(update, context, "Собеседник набирает сообщение...")
+    answer = await chatgpt.add_message(text)
+    await my_message.edit_text(answer)
+
 
 async def date_button(update, context):
     query = update.callback_query.data
-    await send_html(update, context, f"Кнопка {query}")
+    await update.callback_query.answer()
+
+    await send_photo(update, context, query)
+    await send_html(update, context, f"Пригласи за 5 сообщений {dic[query]}")
+
+    prompt = load_prompt(query)
+    chatgpt.set_prompt(prompt)
+
+
+async def message(update, context):
+    dialog.mode = "message"
+    text = load_message("message")
+    await send_photo(update, context, "message")
+    await send_text_buttons(update, context, text, {
+        "message_next": "Следующее сообщение",
+        "message_date": "Пригласить"
+    })
+    dialog.list.clear()
+
+
+async def message_dialog(update, context):
+    text = update.message.text
+    dialog.list.append(text)
+
+
+async def message_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+
+    prompt = load_prompt(query)
+    user_chat_history = "\n\n".join(dialog.list)
+    my_message = await send_text(update, context, "Обработка запроса")
+    for i in range(len(list_think)):
+        time.sleep(0.2)
+        await my_message.edit_text(list_think[i])
+    answer = await chatgpt.send_question(prompt, user_chat_history)
+    await my_message.edit_text(answer)
+
 
 async def hello(update, context):
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
+    if dialog.mode == "date":
+        await date_dialog(update, context)
+    if dialog.mode == "message":
+        await  message_dialog(update, context)
     else:
         await send_text(update, context, "Привет")
-        await send_text(update, context, "Как дела?")
         await send_text(update, context, "Вы написали " + update.message.text)
 
         await send_photo(update, context, "gpt")
@@ -79,17 +128,20 @@ async def hello_buttons(update, context):
 
 dialog = Dialog()
 dialog.mode = None
+dialog.list = []
 
 chatgpt = ChatGptService(
-    token="javcgkAld/r/7U60nS8WDUhWeWVYkZbhjQYpKBFGTvoj5842ast7Pxc54epaCxHRBWXa4vjUutckFaoaUmyOdt62mPPZjjrSFzHlklUvRxjKkD54HiY1iMRLus7TxOkcmPElgqCRPBocX6wJsuWbUTuGkgPNjhYwE08Bvau9oVOiaBcWnUrI/ewY+ccVqx7dnAN4A7RhT46B8BjZjVtU/H8jZakz1cJir+37f/KOL/cTVnmJo=")
+    token="ujavcgkAld/r/7U60nS8WDUhWeWVYkZbhjQYpKBFGTvoj5842ast7Pxc54epaCxHRBWXa4vjUutckFaoaUmyOdt62mPPZjjrSFzHlklUvRxjKkD54HiY1iMRLus7TxOkcmPElgqCRPBocX6wJsuWbUTuGkgPNjhYwE08Bvau9oVOiaBcWnUrI/ewY+ccVqx7dnAN4A7RhT46B8BjZjVtU/H8jZakz1cJir+37f/KOL/cTVnmJo=")
 
 app = ApplicationBuilder().token("7771358337:AAF_6deTgmKK50Be7IG5L7jg2XXqYLpFWhc").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("date", date))
+app.add_handler(CommandHandler("message", message))
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 
 app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
+app.add_handler(CallbackQueryHandler(message_button, pattern="^message_.*"))
 app.add_handler(CallbackQueryHandler(hello_buttons))
 app.run_polling()
